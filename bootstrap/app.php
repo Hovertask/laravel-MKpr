d8b8c75c8b680270e8f15925582c7b71ffbbe740
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-04-11 19:28:28 +0100

    consolidated

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'wallet/initialize-payment',
+            'wallet/verify-payment/*', // For your verify-payment route
+            'webhook/paystack',        // Example for Paystack webhook
+        ]);
+        
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
