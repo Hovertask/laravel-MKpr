a9393f712aeba6924649a6e8d9ac3d0e8898ba79
Adelakun Ahmad <adelakunahmad@gmail.com>
2025-10-07 03:16:52 -0700

    fixed cors errors

--- config/cors.php
+++ config/cors.php
@@ -4,12 +4,13 @@ return [
     'paths' => ['api/*', 'login', 'logout', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        'http://localhost:5173',
-        'https://hovertask.com',
-        'https://backend.hovertask.com',
-        'https://app.hovertask.com',
-        'http://app.hovertask.com',
-    ],
+    'http://localhost:5173',
+    'https://hovertask.com',
+    'https://www.hovertask.com',
+    'https://backend.hovertask.com',
+    'https://app.hovertask.com',
+    'http://app.hovertask.com',
+  ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
@@ -18,14 +19,3 @@ return [
 ];
 
 
-// return [
-//     'paths' => ['api/*', 'login', 'logout', 'sanctum/csrf-cookie'], // Define routes that should allow CORS
-//     'allowed_methods' => ['*'],
-//     'allowed_origins' => ['*'],  // You can specify a domain instead of '*'
-//     'allowed_origins_patterns' => [],
-//     'allowed_headers' => ['*'],
-//     'exposed_headers' => [],
-//     'max_age' => 0,
-//     'supports_credentials' => true,
-
-// ];
