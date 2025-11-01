f43f2e24913328307a66bf416d792089f630946e
Adelakun Ahmad <adelakunahmad@gmail.com>
2025-11-01 00:01:39 -0700

    fixed bug

--- app/Repository/WalletRepository.php
+++ app/Repository/WalletRepository.php
@@ -136,8 +136,7 @@ class WalletRepository implements IWalletRepository
         $superAdminId = \DB::table('users')
             ->join('role_user', 'users.id', '=', 'role_user.user_id')
             ->join('roles', 'roles.id', '=', 'role_user.role_id')
-            ->where('roles.fname', 'superadministrator')
-            ->orWhere('roles.lname', 'superadmin')
+            ->where('roles.name', 'superadministrator')
             ->value('users.id');
 
             // ✅ Log for debugging
