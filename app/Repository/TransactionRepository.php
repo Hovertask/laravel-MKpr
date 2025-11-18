80a590bede6ceff532fd8d6c9acdbd55067d07e2
Barnabas <codedkolobanny@gmail.com>
2025-11-18 10:50:38 +0000

    add transaction features

---
+++ app/Repository/TransactionRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repository;
+
+class TransactionRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
