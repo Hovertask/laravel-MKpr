80d8a6caea1cea7edde0f2f6f17a3801ee267641
Adelakun Ahmad <adelakunahmad@gmail.com>
2025-11-18 04:35:51 -0800

    fix desc error

--- app/Repository/TransactionRepository.php
+++ app/Repository/TransactionRepository.php
@@ -14,7 +14,7 @@ class TransactionRepository
     public function getTransactionsForUser(int $userId): Collection
     {
         return Transaction::where('user_id', $userId)
-            ->orderBy('date', 'desc')
+            ->orderBy('created_at', 'desc')
             ->get();
     }
 
