1cf5d956e008a59e8940675dea80cfabae389e62
Adelakun Ahmad <adelakunahmad@gmail.com>
2025-11-20 02:41:43 -0800

    add  use App\Http\Controllers\Controller to ResellerConversionRepository;

--- app/Http/Controllers/Api/V1/ResellerConversionController.php
+++ app/Http/Controllers/Api/V1/ResellerConversionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Repository\ResellerConversionRepository;
 use App\Models\Product;
 
