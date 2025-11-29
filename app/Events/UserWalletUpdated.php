770783e60031d9a0f979e6caf0c85908071adad3
Adelakun Ahmad <adelakunahmad@gmail.com>
2025-11-29 04:23:16 -0800

    fix broadcast error

--- app/Events/UserWalletUpdated.php
+++ app/Events/UserWalletUpdated.php
@@ -8,6 +8,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
 class UserWalletUpdated implements ShouldBroadcastNow
 {
