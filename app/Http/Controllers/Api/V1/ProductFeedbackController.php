b7789a4ea73f250083a3817e8f885516f78648d7
Adelakun Ahmad <adelakunahmad@gmail.com>
2025-12-10 14:55:27 -0800

    fix controller error

--- app/Http/Controllers/Api/V1/ProductFeedbackController.php
+++ app/Http/Controllers/Api/V1/ProductFeedbackController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Requests\StoreFeedbackRequest;
-use App\Repositories\ProductFeedbackRepository;
+use App\Repository\ProductFeedbackRepository;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\RateLimiter;
 
