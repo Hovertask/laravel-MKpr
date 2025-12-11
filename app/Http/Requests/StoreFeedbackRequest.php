6766bab7a060ca79eb2d8aacfac66f38525b372e
Adelakun Ahmad <adelakunahmad@gmail.com>
2025-12-11 01:47:31 -0800

    fix request

--- app/Http/Requests/StoreFeedbackRequest.php
+++ app/Http/Requests/StoreFeedbackRequest.php
@@ -8,18 +8,24 @@ class StoreFeedbackRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return true; // anyone can leave feedback
+        return true;
+    }
+
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'auth' => auth()->check(),
+        ]);
     }
 
     public function rules(): array
     {
         return [
-            'rating' => 'required|integer|min:1|max:5',
-            'comment' => 'required|string|min:5',
+            'rating'        => 'required|integer|min:1|max:5',
+            'comment'       => 'required|string|min:5',
 
-            // Required if user is NOT authenticated
-            'visitor_name'  => 'required_without:user_id|string|max:120',
-            'visitor_email' => 'required_without:user_id|email|max:120',
+            'visitor_name'  => 'required_unless:auth,true|string|max:120',
+            'visitor_email' => 'required_unless:auth,true|email|max:120',
         ];
     }
 }
